fe5096dc768061c45b5f917f72a52848bb14e63f
stx18852277447` <1071852424@qq.com>
2023-11-21 00:05:40 +0800

    制作 Vue 公共组件

---
+++ src/components/the-header.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-layout-header class="header">
+    <div class="logo">
+      您好：{{ member.mobile }}
+      <router-link to="/login" style="color: white">退出登录</router-link>
+    </div>
+    <a-menu
+      v-model:selectedKeys="selectedKeys1"
+      theme="dark"
+      mode="horizontal"
+      :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import store from '@/store';
+
+let member = store.state.member;
+
+const selectedKeys1 = ref(["2"]);
+</script>
+
+<style scoped>
+</style>
