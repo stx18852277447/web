468d3392f15be6557cf7074252f7318d2c9f5906
stx18852277447` <1071852424@qq.com>
2023-11-21 00:04:21 +0800

    增加 Web 控台主页

---
+++ src/views/main.vue
@@ -0,0 +1,51 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <the-header-view></the-header-view>
+    <a-layout>
+      <the-sider-view></the-sider-view>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          总会员数：{{ count }}
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script setup>
+import TheHeaderView from "@/components/the-header";
+import TheSiderView from "@/components/the-sider.vue";
+import { ref } from "vue";
+import axios from "axios";
+
+const count = ref(0);
+axios.get("/member/member/count").then((response) => {
+  count.value = response.data;
+})
+</script>
+
+<style scoped>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
