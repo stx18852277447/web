991b2117998576c47797a73da399f04797110300
stx18852277447` <1071852424@qq.com>
2023-11-21 18:09:04 +0800

    乘车人编辑页面开发

--- src/views/main/passenger.vue
+++ src/views/main/passenger.vue
@@ -1,131 +1,169 @@
 <template>
     <p>
-        <a-space>
-            <a-button type="primary" @click="handleQuery">刷新</a-button>
-            <a-button type="primary" @click="showModal">新增</a-button>
-        </a-space>
+      <a-space>
+        <a-button type="primary" @click="handleQuery()">刷新</a-button>
+        <a-button type="primary" @click="onAdd">新增</a-button>
+      </a-space>
     </p>
-    <a-table :dataSource="passengers" :columns="columns" :pagination="pagination" @chage="handleTableChange" />
-    <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
-        <a-form :model="passenger" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
-            <a-form-item label="姓名">
-                <a-input v-model:value="passenger.name" />
-            </a-form-item>
-            <a-form-item label="身份证">
-                <a-input v-model:value="passenger.idCard" />
-            </a-form-item>
-            <a-form-item label="类型">
-                <a-select v-model:value="passenger.type">
-                    <a-select-option value="1">成人</a-select-option>
-                    <a-select-option value="2">儿童</a-select-option>
-                    <a-select-option value="3">学生</a-select-option>
-                </a-select>
-            </a-form-item>
-        </a-form>
+    <a-table
+      :dataSource="passengers"
+      :columns="columns"
+      :pagination="pagination"
+      @change="handleTableChange"
+    >
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.dataIndex === 'operation'">
+          <a-space>
+            <a @click="onEdit(record)">编辑</a>
+          </a-space>
+        </template>
+      </template>
+    </a-table>
+  
+    <a-modal
+      v-model:visible="visible"
+      title="乘车人"
+      @ok="handleOk"
+      ok-text="确认"
+      cancel-text="取消"
+    >
+      <a-form
+        :model="passenger"
+        :label-col="{ span: 4 }"
+        :wrapper-col="{ span: 20 }"
+      >
+        <a-form-item label="姓名">
+          <a-input v-model:value="passenger.name" />
+        </a-form-item>
+        <a-form-item label="身份证">
+          <a-input v-model:value="passenger.idCard" />
+        </a-form-item>
+        <a-form-item label="类型">
+          <a-select v-model:value="passenger.type">
+            <a-select-option value="1">成人</a-select-option>
+            <a-select-option value="2">儿童</a-select-option>
+            <a-select-option value="3">学生</a-select-option>
+          </a-select>
+        </a-form-item>
+      </a-form>
     </a-modal>
-</template>    
-<script setup>
-import { ref, reactive, onMounted } from "vue";
-import { notification } from "ant-design-vue";
-import axios from "axios";
-
-const visible = ref(false);
-
-const passenger = reactive({
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import { notification } from 'ant-design-vue';
+  import axios from 'axios';
+  
+  const visible = ref(false);
+  
+  const passenger = ref({
     id: undefined,
     memberId: undefined,
     name: undefined,
     idCard: undefined,
     type: undefined,
     createTime: undefined,
-    updateTime: undefined,
-});
-
-const showModal = () => {
+    updateTime: undefined
+  });
+  
+  const onAdd = () => {
     visible.value = true;
-};
-const handleOk = () => {
-    axios.post("/member/passenger/save", passenger).then((response) => {
-        let data = response.data;
-        if (data.success) {
-            notification.success({ description: "保存成功!" });
-            visible.value = false;
-            handleQuery ({
-                page: pagination.current,
-                size: pagination.pageSize
-            });
-        } else {
-            notification.error({ description: data.message });
-        }
+  };
+  
+  const onEdit = (record) => {
+    // passenger.value = record;
+    passenger.value = window.Tool.copy(record);
+    visible.value = true;
+  };
+  
+  const handleOk = () => {
+    axios.post('/member/passenger/save', passenger.value).then((response) => {
+      let data = response.data;
+      if (data.success) {
+        notification.success({ description: '保存成功！' });
+        visible.value = false;
+        handleQuery({
+          page: pagination.value.current,
+          size: pagination.value.pageSize
+        });
+      } else {
+        notification.error({ description: data.message });
+      }
     });
-};
-
-const passengers = ref([]);
-
-const pagination = reactive({
+  };
+  
+  const passengers = ref([]);
+  
+  // 分页的三个属性名是固定的
+  const pagination = ref({
     total: 0,
     current: 1,
-    pagesize: 2,
-});
-
-const columns = [
+    pageSize: 3
+  });
+  
+  const columns = [
     {
-        title: "姓名",
-        dataIndex: "name",
-        key: "name",
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name'
     },
     {
-        title: "身份证",
-        dataIndex: "idCard",
-        key: "idcard",
+      title: '身份证',
+      dataIndex: 'idCard',
+      key: 'idCard'
     },
     {
-        title: "类型",
-        dataIndex: "type",
-        key: "type",
+      title: '类型',
+      dataIndex: 'type',
+      key: 'type'
     },
-];
-
-const handleQuery = (param) => {
-    if (!param) {
-        param = {
-            page: 1,
-            size: pagination.pageSize,
-        };
-    }    
-    axios
-        .get("/member/passenger/query-list", {
-            params: {
-                page: param.page,
-                size: param.size,
-            },
-        })
-        .then((response) => {
-            let data = response.data;
-            if (data.success) {
-                passengers.value = data.content.list;
-                //设置分页控件的值
-                pagination.current = param.page;
-                pagination.total = data.content.total;
-            } else {
-                notification.error({ description: data.message });
-            }
-        });
-};
-
-onMounted(() => {
+    {
+      title: '操作',
+      dataIndex: 'operation'
+    }
+  ];
+  
+  const handleTableChange = (pagination) => {
     handleQuery({
+      page: pagination.current,
+      size: pagination.pageSize
+    });
+  };
+  
+  const handleQuery = (param) => {
+    if (!param) {
+      param = {
         page: 1,
-        size: pagination.pagesize,
+        size: pagination.value.pageSize
+      };
+    }
+  
+    axios
+      .get('/member/passenger/query-list', {
+        params: {
+          page: param.page,
+          size: param.size
+        }
+      })
+      .then((response) => {
+        let data = response.data;
+        if (data.success) {
+          passengers.value = data.content.list;
+          // 设置分页控件的值
+          pagination.value.current = param.page;
+          pagination.value.total = data.content.total;
+        } else {
+          notification.error({ description: data.message });
+        }
+      });
+  };
+  
+  onMounted(() => {
+    handleQuery({
+      page: 1,
+      size: pagination.value.pageSize
     });
-});
-
-const handleTableChange = (pagination) => {
-    console.log("看看自带的分页参数都有啥:" + pagination); handleQuery({
-        page: pagination.current, size: pagination.pageSize,
-    });
-};
-
-</script>
-
-<style scoped></style>
+  });
+  </script>
+  
+  <style scoped></style>
