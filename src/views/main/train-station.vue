a59bf84fae8b3b36e9ed739b040060c924be5cff
stx18852277447` <1071852424@qq.com>
2023-11-22 10:28:12 +0800

    车次表单增加车站下拉选择

--- src/views/main/train-station.vue
+++ src/views/main/train-station.vue
@@ -21,7 +21,13 @@
     <a-modal v-model:visible="visible" title="火车车站" @ok="handleOk" ok-text="确认" cancel-text="取消">
         <a-form :model="trainStation" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
             <a-form-item label="车次编号">
-                <a-input v-model:value="trainStation.trainCode" />
+                <a-select v-model:value="trainStation.trainCode" show-search :filter0ption="filterTrainCode0ption">
+                    <a-select-option v-for="item in trains" :key="item.code" :value="item.code"
+                        :label="item.code + item.start + item.end">
+                        {{ item.code }} | {{ item.start }} ~ {{ item.end }}
+                    </a-select-option>
+                </a-select>
+
             </a-form-item>
             <a-form-item label="站序">
                 <a-input v-model:value="trainStation.index" />
@@ -210,10 +216,27 @@ const handleTableChange = (pagination) => {
     });
 };
 
+const queryTrainCode = () => {
+    axios.get('/business/admin/train/query-all').then((response) => {
+        let data = response.data;
+        if (data.success) {
+            console.log(data.content);
+            trains.value = data.content;
+        } else {
+            notification.error({ description: data.message });
+        }
+    });
+};
+const trains = ref([]);
+const filterTrainCodeOption = (input, option) => {
+    console.log(input, option);
+    return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+};
 onMounted(() => {
     handleQuery({
         page: 1,
         size: pagination.value.pageSize
     });
+    queryTrainCode();
 });
 </script>
