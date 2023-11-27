acd85e82bd42cab1c83df59490b271ec584cdf98
stx18852277447` <1071852424@qq.com>
2023-11-27 16:17:56 +0800

    选座成功后会会员增加车票记录

--- src/components/the-sider.vue
+++ src/components/the-sider.vue
@@ -1,64 +1,73 @@
 <template>
   <a-layout-sider width="200" style="background: #fff">
-    <a-menu
-        v-model:selectedKeys="selectedKeys"
-        :openKeys="['batch', 'base','business']"
-        mode="inline"
-        :style="{ height: '100%', borderRight: 0 }"
-    >
+    <a-menu v-model:selectedKeys="selectedKeys" :openKeys="['batch', 'base', 'business']" mode="inline"
+      :style="{ height: '100%', borderRight: 0 }">
       <a-menu-item key="/welcome">
         <router-link to="/welcome">
-          <coffee-outlined/>
+          <coffee-outlined />
           欢迎
         </router-link>
       </a-menu-item>
       <a-menu-item key="/about">
         <router-link to="/about">
-          <user-outlined/>
+          <user-outlined />
           关于
         </router-link>
       </a-menu-item>
-
+      <a-sub-menu key="member">
+        <template #title>
+          <span>
+            <UnorderedListOutlined />
+            会员管理
+          </span>
+        </template>
+        <a-menu-item key="/member/ticket">
+          <router-link to="/member/ticket">
+            <MenuUnfoldOutlined />
+            会员车票
+          </router-link>
+        </a-menu-item>
+      </a-sub-menu>
       <a-sub-menu key="batch">
         <template #title>
           <span>
-            <UnorderedListOutlined/>
+            <UnorderedListOutlined />
             业务管理
           </span>
         </template>
         <a-menu-item key="/business/confirm-order">
           <router-link to="/business/confirm-order">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             订单信息
           </router-link>
         </a-menu-item>
         <a-menu-item key="/business/daily-train-ticket">
           <router-link to="/business/daily-train-ticket">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             余票信息
           </router-link>
         </a-menu-item>
         <a-menu-item key="/business/daily-train">
           <router-link to="/business/daily-train">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             每日车次
           </router-link>
         </a-menu-item>
         <a-menu-item key="/business/daily-train-station">
           <router-link to="/business/daily-train-station">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             每日车站
           </router-link>
         </a-menu-item>
         <a-menu-item key="/business/daily-train-carriage">
           <router-link to="/business/daily-train-carriage">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             每日车厢
           </router-link>
         </a-menu-item>
         <a-menu-item key="/business/daily-train-seat">
           <router-link to="/business/daily-train-seat">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             每日座位
           </router-link>
         </a-menu-item>
@@ -66,13 +75,13 @@
       <a-sub-menu key="stu">
         <template #title>
           <span>
-            <UnorderedListOutlined/>
+            <UnorderedListOutlined />
             学生管理
           </span>
         </template>
         <a-menu-item key="/stu/stx-stu">
           <router-link to="/stu/stx-stu">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             软件2242
           </router-link>
         </a-menu-item>
@@ -80,13 +89,13 @@
       <a-sub-menu key="batch">
         <template #title>
           <span>
-            <UnorderedListOutlined/>
+            <UnorderedListOutlined />
             跑批管理
           </span>
         </template>
         <a-menu-item key="/batch/job">
           <router-link to="/batch/job">
-            <MenuUnfoldOutlined/>
+            <MenuUnfoldOutlined />
             任务管理
           </router-link>
         </a-menu-item>
@@ -94,37 +103,37 @@
       <a-sub-menu key="base">
         <template #title>
           <span>
-            <UnorderedListOutlined/>
+            <UnorderedListOutlined />
             基础数据
           </span>
         </template>
         <a-menu-item key="/base/station">
           <router-link to="/base/station">
-            <car-outlined/>
+            <car-outlined />
             车站管理
           </router-link>
         </a-menu-item>
         <a-menu-item key="/base/train">
           <router-link to="/base/train">
-            <car-outlined/>
+            <car-outlined />
             火车管理
           </router-link>
         </a-menu-item>
         <a-menu-item key="/base/train-station">
           <router-link to="/base/train-station">
-            <car-outlined/>
+            <car-outlined />
             火车车站
           </router-link>
         </a-menu-item>
         <a-menu-item key="/base/train-carriage">
           <router-link to="/base/train-carriage">
-            <car-outlined/>
+            <car-outlined />
             火车车厢
           </router-link>
         </a-menu-item>
         <a-menu-item key="/base/train-seat">
           <router-link to="/base/train-seat">
-            <car-outlined/>
+            <car-outlined />
             火车座位
           </router-link>
         </a-menu-item>
@@ -134,18 +143,18 @@
 </template>
 
 <script setup>
-import {ref, watch} from 'vue';
+import { ref, watch } from 'vue';
 import router from '@/router';
 
 const selectedKeys = ref([]);
 
 watch(
-    () => router.currentRoute.value.path,
-    (newValue) => {
-      console.log('watch', newValue);
-      selectedKeys.value = [];
-      selectedKeys.value.push(newValue);
-    },
-    {immediate: true}
+  () => router.currentRoute.value.path,
+  (newValue) => {
+    console.log('watch', newValue);
+    selectedKeys.value = [];
+    selectedKeys.value.push(newValue);
+  },
+  { immediate: true }
 );
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,100 +1,114 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('../views/main.vue'),
+    path: "/",
+    component: () => import("../views/main.vue"),
     children: [
       {
-        path: 'welcome',
-        component: () => import('../views/main/welcome.vue')
+        path: "welcome",
+        component: () => import("../views/main/welcome.vue"),
       },
       {
-        path: 'about',
-        component: () => import('../views/main/about.vue')
-      },{
-        path: 'business/',
-        children: [
-          {
-            path: 'daily-train',
-            component: () => import('../views/main/business/daily-train.vue')
-          },
-          {
-            path: 'daily-train-station',
-            component: () => import('../views/main/business/daily-train-station.vue')
-          },
-          {
-            path: 'daily-train-carriage',
-            component: () => import('../views/main/business/daily-train-carriage.vue')
-          },
-          {
-            path: 'daily-train-seat',
-            component: () => import('../views/main/business/daily-train-seat.vue')
-          },
-          {
-            path: 'daily-train-ticket',
-            component: () => import('../views/main/business/daily-train-ticket.vue')
-          },
-          {
-            path: 'confirm-order',
-            component: () => import('../views/main/business/confirm-order.vue')
-          }
-        ]
+        path: "about",
+        component: () => import("../views/main/about.vue"),
       },
       {
-        path: 'base/',
+        path: "member",
         children: [
           {
-            path: 'station',
-            component: () => import('../views/main/base/station.vue')
+            path: "ticket",
+            component: () => import("../views/main/member/ticket.vue"),
           },
-          {
-            path: 'train',
-            component: () => import('../views/main/base/train.vue')
-          },
-          {
-            path: 'train-station',
-            component: () => import('../views/main/base/train-station.vue')
-          },
-          {
-            path: 'train-carriage',
-            component: () => import('../views/main/base/train-carriage.vue')
-          },
-          {
-            path: 'train-seat',
-            component: () => import('../views/main/base/train-seat.vue')
-          }
-        ]
+        ],
       },
       {
-        path: 'stu/',
+        path: "business/",
         children: [
           {
-            path: 'stx-stu',
-            component: () => import('../views/main/stu/stx-stu.vue')
-          }
-        ]
+            path: "daily-train",
+            component: () => import("../views/main/business/daily-train.vue"),
+          },
+          {
+            path: "daily-train-station",
+            component: () =>
+              import("../views/main/business/daily-train-station.vue"),
+          },
+          {
+            path: "daily-train-carriage",
+            component: () =>
+              import("../views/main/business/daily-train-carriage.vue"),
+          },
+          {
+            path: "daily-train-seat",
+            component: () =>
+              import("../views/main/business/daily-train-seat.vue"),
+          },
+          {
+            path: "daily-train-ticket",
+            component: () =>
+              import("../views/main/business/daily-train-ticket.vue"),
+          },
+          {
+            path: "confirm-order",
+            component: () => import("../views/main/business/confirm-order.vue"),
+          },
+        ],
       },
       {
-        path: 'batch/',
+        path: "base/",
         children: [
           {
-            path: 'job',
-            component: () => import('../views/main/batch/job.vue')
-          }
-        ]
-      }
-    ]
+            path: "station",
+            component: () => import("../views/main/base/station.vue"),
+          },
+          {
+            path: "train",
+            component: () => import("../views/main/base/train.vue"),
+          },
+          {
+            path: "train-station",
+            component: () => import("../views/main/base/train-station.vue"),
+          },
+          {
+            path: "train-carriage",
+            component: () => import("../views/main/base/train-carriage.vue"),
+          },
+          {
+            path: "train-seat",
+            component: () => import("../views/main/base/train-seat.vue"),
+          },
+        ],
+      },
+      {
+        path: "stu/",
+        children: [
+          {
+            path: "stx-stu",
+            component: () => import("../views/main/stu/stx-stu.vue"),
+          },
+        ],
+      },
+      {
+        path: "batch/",
+        children: [
+          {
+            path: "job",
+            component: () => import("../views/main/batch/job.vue"),
+          },
+        ],
+      },
+    ],
   },
   {
-    path: '',
-    redirect: '/welcome'
-  }
+    path: "",
+    redirect: "/welcome",
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
